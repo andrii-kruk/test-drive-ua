2fad9f43a83b8fb280f294372c2dc295975f52e4
Andrii Kruk <119102553+chodgi71@users.noreply.github.com>
2023-11-04 14:27:24 +0100

    HomePage content

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,9 +1,26 @@
-
-
 const MainPage = () => {
   return (
-    <>
-    </>
+    <section>
+      <h2>About Us</h2>
+      <p>
+        Welcome to our car rental service! We're dedicated to providing you with
+        a convenient and flexible way to rent a car by the hour. Whether you're
+        looking for a quick ride around the city or need a reliable vehicle for
+        a short-term trip, we've got you covered.
+      </p>
+      <p>
+        Our mission is to make car rental as easy as possible. With a simple
+        registration process and a user-friendly app, you can book a car in just
+        a few clicks. Our diverse fleet of vehicles ensures that you'll find the
+        perfect car to suit your needs, from compact cars to spacious SUVs.
+      </p>
+      <p>
+        Customer satisfaction is our top priority, and we take pride in
+        delivering clean, well-maintained cars. You can rely on us for
+        transparent pricing, hassle-free pickups, and a seamless return process.
+        Discover the freedom of hourly car rentals with us!
+      </p>
+    </section>
   );
 };
 
